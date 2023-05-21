6b3dd91db3c1a9b69ab30b00684eba8f171f68f2
saymanrifat <98202630+saymanrifat@users.noreply.github.com>
2023-05-21 17:12:30 +0600

    BrodeCastStream Added

--- lib/main.dart
+++ lib/main.dart
@@ -28,27 +28,55 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   int counter = 0;
   StreamController counterController = StreamController();
+  late Stream mySteam;
+
+  @override
+  void initState() {
+    super.initState();
+    mySteam = counterController.stream.asBroadcastStream();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
           child: Center(
-        child: StreamBuilder(
-          stream: counterController.stream,
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
-              return Text(
-                snapshot.data.toString(),
-                style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
-              );
-            } else {
-              return Text(
-                '0',
-                style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
-              );
-            }
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            StreamBuilder(
+              stream: mySteam,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    snapshot.data.toString(),
+                    style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                  );
+                } else {
+                  return Text(
+                    '0',
+                    style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                  );
+                }
+              },
+            ),
+            StreamBuilder(
+              stream: mySteam,
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return Text(
+                    snapshot.data.toString(),
+                    style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                  );
+                } else {
+                  return Text(
+                    '0',
+                    style: TextStyle(fontSize: 70, fontWeight: FontWeight.bold),
+                  );
+                }
+              },
+            ),
+          ],
         ),
       )),
       floatingActionButton: FloatingActionButton(
